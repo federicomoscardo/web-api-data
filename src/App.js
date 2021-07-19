7860ec42c7eee0302f6a85afd7bfa63c2907c216
federicomoscardo <federico.moscardo@gmail.com>
2021-07-19 16:53:08 -0300

    imported React-Router to this app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
 import './App.css';
+
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div>
+      <Router>
+
+      </Router>
       
     </div>
   );
