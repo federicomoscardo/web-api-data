382450e154981ce165fbbac18b81067d534810c8
federicomoscardo <federico.moscardo@gmail.com>
2021-07-19 17:11:18 -0300

    created the structure of React-Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
-
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import WebNavbar from './components/WebNavbar'
+import Community from './pages/Community'
 
 function App() {
   return (
     <div>
       <Router>
+        <WebNavbar></WebNavbar>
 
+        <Switch>
+          <Route path="/community" exact>
+            <Community />
+          </Route>
+
+          <Route>
+            
+          </Route>
+        </Switch>
       </Router>
       
     </div>
