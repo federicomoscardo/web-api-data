f6f51d300d106d5747db4d64e880fe808ceb663d
federicomoscardo <federico.moscardo@gmail.com>
2021-07-21 17:24:43 -0300

    added to BrowserRouter the component Products

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Container} from 'react-bootstrap'
 
 import WebNavbar from './components/WebNavbar'
 import Community from './pages/Community'
+import Products from './pages/Products'
 
 function App() {
   return (
-    <div>
+    <Container fluid>
       <Router>
         <WebNavbar></WebNavbar>
-
+          
         <Switch>
           <Route path="/community" exact>
             <Community />
           </Route>
 
-          <Route>
-            
+          <Route path="/products" exact>
+            <Products />
           </Route>
         </Switch>
       </Router>
       
-    </div>
+    </Container>
   );
 }
 
