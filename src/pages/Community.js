528c4f5df752f476a8f61918fcb71a17c2bda2c4
federicomoscardo <federico.moscardo@gmail.com>
2021-07-21 17:02:15 -0300

    created the component, the connection to the API and the structure of the data table

--- src/pages/Community.js
+++ src/pages/Community.js
@@ -1,8 +1,38 @@
-
+import { useEffect, useState } from 'react';
+import {Container, Row, Col} from 'react-bootstrap'
 
 function Community() {
+    let [users, setUsers] = useState({data:[]})
+
+    useEffect(function() {
+        fetch('https://fakerapi.it/api/v1/users?_seed=12456&_quantity=200')
+            .then(result => result.json())
+            .then(json => setUsers(json))
+    }, [])
+
     return (
-        <p>Hello World!</p>
+        <Container>
+                    <Row className="table-title">
+                        <Col>Username</Col>
+                        <Col>Name and Surname</Col>
+                        <Col>Email</Col>
+                        <Col>Website</Col>
+                    </Row>
+                    {
+                        users.data.map(user => {
+                            return (
+                                <>
+                                    <Row key={user.uuid} className="row-data">
+                                        <Col><img src={user.image} className="image-user"></img>{user.username}</Col>
+                                        <Col>{user.firstname} {user.lastname}</Col>
+                                        <Col>{user.email}</Col>
+                                        <Col>{user.website}</Col>
+                                    </Row>
+                                </>
+                            )
+                        })
+                    }
+        </Container>
     )
 }
 
