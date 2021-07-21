949500590bf31bc1cb500fa561d61f697d9f0cc0
federicomoscardo <federico.moscardo@gmail.com>
2021-07-21 17:04:20 -0300

    set the HREF to Community link and home

--- src/components/WebNavbar.js
+++ src/components/WebNavbar.js
@@ -4,11 +4,11 @@ function WebNavbar() {
     return (
             <Navbar bg="light" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">MyWeb (API Test)</Navbar.Brand>
+                    <Navbar.Brand href="/">MyWeb (API Test)</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Community</Nav.Link>
+                        <Nav.Link href="/community">Community</Nav.Link>
                         <Nav.Link href="#link">Link</Nav.Link>
                     </Nav>
                     </Navbar.Collapse>
